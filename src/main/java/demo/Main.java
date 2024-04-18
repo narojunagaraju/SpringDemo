b413186e68189c19d0f74e7ec45be115a7e73c95
narojunagaraju <narojunagarajun@gmail.com>
2024-04-18 10:37:10 +0530

    config injection

--- src/main/java/demo/Main.java
+++ src/main/java/demo/Main.java
@@ -1,19 +1,14 @@
 package demo;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
 
-        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
-
-        Doctor doctor = context.getBean(Doctor.class);
-        doctor.assist();
-
-
-        Nurse nurse = context.getBean(Nurse.class);
-        nurse.assist();
+        ApplicationContext context = new AnnotationConfigApplicationContext(BeanConfig.class);
+        Staff staff = context.getBean(Doctor.class);
+        staff.assist();
 
     }
 }
